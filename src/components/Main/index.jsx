fe3bbea5885d767fee636b59239e00b208befcd1
Guilherme <guiduck02@gmail.com>
2021-10-08 23:52:45 -0300

    useGeolocation hook is working

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import useGeoLocation from '../../hooks/useGeoLocation'
+
 
 import { Container } from './styles'
 
 export default function Main() {
+
+  const location = useGeoLocation();
+
   return (
     <Container>
+      hallo world
 
+      {
+        location.loaded ? JSON.stringify(location) : 'location is not available'
+      }
     </Container>
   )
 }
---
+++ src/hooks/useGeoLocation.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+
+const useGeoLocation = () => {
+  const [location, setLocation] = useState({
+    loaded: false,
+    coordinates: { lat: '', lon: ''},
+  });
+
+  const onSuccess = (location) => {
+    setLocation({
+      loaded: true,
+      coordinates: {
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+      },
+    });
+  };
+
+  const onError = (error) => {
+    setLocation({
+      loaded: true,
+      error,
+    });
+  }
+
+  useEffect(() => {
+    if(navigator.geolocation) {
+      onError({
+        code: 0,
+        message: "geolocation not supported"
+      });
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError)
+  }, [])
+
+  return location;
+}
+
+export default useGeoLocation;
